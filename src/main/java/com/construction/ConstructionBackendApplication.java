9e26275534fe738f1ec1cbfb07c12eef765e1ce5
jeonyoungho <yhjun1000@gmail.com>
2022-03-15 00:23:15 +0900

    sprint boot version 2.6.4 -> 2.5.4 로 변경

---
+++ src/main/java/com/construction/ConstructionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.construction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionBackendApplication.class, args);
+	}
+
+}
